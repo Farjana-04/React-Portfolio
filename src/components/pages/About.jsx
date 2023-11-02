310e67b92b963ef899cbe7f21407ccfa79c3c3f7
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-02 17:58:43 -0500

    Created folders and files, and installed dependecies and packages

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,23 @@
+// This is a static page mocking an "About Us" section for our fake user data
+// import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      {/* <p className="content is-medium"> Full Stack Developer </p> */}
+      
+      
+      <p className="content is-italic mt-4">
+      Welcome to my page! This is Farjana Akhter. I recently started coding bootcamp from University of Texas at
+      Austin.
+      </p>
+      <p className="content">
+      I'm making my entry into the world of web development as I have found a passion for problem
+      solving on interesting projects through the use of code.
+      During the course we are learning usable, professional websites using best practice accessibility
+      HTML, CSS and JavaScript.
+      </p>
+    </div>
+  );
+}
+
---
+++ src/seed.jsx
@@ -0,0 +1,20 @@
+let seedData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "FA",
+    "role": " Developer",
+    "linkedinId":"Your LinkedIn Id",
+    
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/farjana-akhter-ab328a283/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Farjana-04",
+          "className":"fa fa-github"
+        },
+       
+      ],
+    }
