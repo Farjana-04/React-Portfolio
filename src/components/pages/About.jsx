077461f5571949ee6f8713e159e9f8ada27a1540
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-06 16:15:35 -0600

    added code to write about me and set the candidate photo in src/components/pages/About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,22 +1,22 @@
 
-// import React from 'react';
+import React from 'react';
 
-// export default function About() {
-//   return (
-//     <div className="container mt-5">
-//       <div className="row">
-//         <div className="col-md-4">
-//           <img src="/assets/me0.jpeg" alt="Candidate Photo" className="img-fluid rounded-circle" />
-//         </div>
-//         <div className="col-md-8">
-//           <p>
-//           Welcome to my page! This is Farjana Akhter. I recently started coding bootcamp from University of Texas at Austin.
-//           I'm making my entry into the world of web development as I have found a passion for problem solving on interesting projects through the use of code. During the course, we are learning usable, professional websites using best practice accessibility HTML, CSS, and JavaScript.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+export default function About() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="/assets/me0.jpeg" alt="Candidate Photo" className="img-fluid rounded-circle" />
+        </div>
+        <div className="col-md-8">
+          <p>
+          Welcome to my page! This is Farjana Akhter. I recently started coding bootcamp from University of Texas at Austin.
+          I'm making my entry into the world of web development as I have found a passion for problem solving on interesting projects through the use of code. During the course, we are learning usable, professional websites using best practice accessibility HTML, CSS, and JavaScript.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 
