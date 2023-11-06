488e26a88dd1f1d29899b4852f3bc77d5aec2745
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-06 16:46:28 -0600

    Created footer file and coded to click the github and linkedIn icon

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
+import React from 'react';
 
-// const Footer = () => {
-//   return (
+const Footer = () => {
+  return (
     
-//     <footer className="footer fixed-bottom bg-light text-light py-3">
-//       <div className="container text-center">
-//         <a href="https://github.com/Farjana-04" target="_blank" rel="noopener noreferrer">
-//         <img src="/assets/git.png" alt="GitHub" className="icon" />
+    <footer className="footer fixed-bottom bg-light text-light py-3">
+      <div className="container text-center">
+        <a href="https://github.com/Farjana-04" target="_blank" rel="noopener noreferrer">
+        <img src="/assets/git.png" alt="GitHub" className="icon" />
           
-//         </a>
-//         <span className="mx-2">|</span>
-//         <a href="https://www.linkedin.com/in/farjana-akhter-ab328a283/" target="_blank" rel="noopener noreferrer">
-//         <img src="/assets/linkedIn.png" alt="LinkedIn" className="icon" />
-//         </a>
-//       </div>
-//     </footer>
-//     );
-//   };
+        </a>
+        <span className="mx-2">|</span>
+        <a href="https://www.linkedin.com/in/farjana-akhter-ab328a283/" target="_blank" rel="noopener noreferrer">
+        <img src="/assets/linkedIn.png" alt="LinkedIn" className="icon" />
+        </a>
+      </div>
+    </footer>
+    );
+  };
   
 
 
-// export default Footer;
+export default Footer;
