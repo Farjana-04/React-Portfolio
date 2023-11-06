8db4c83bb5482b2e3e97b7a08dff27a996549312
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-06 16:11:47 -0600

    created a new root by selecting the DOM element with the ID root, and can use to render the app in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+//creates a new root by selecting the DOM element with the ID "root." It returns a Root object that we can use to render the app.
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
